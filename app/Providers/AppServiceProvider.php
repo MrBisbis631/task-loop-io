0f616309e6a4089d879d1b660c20ab341a7bf4e4
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2025-03-28 04:06:04 +0300

    change: disable wrapping for JSON resources in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -16,5 +17,8 @@ final class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+        JsonResource::withoutWrapping();
+    }
 }
